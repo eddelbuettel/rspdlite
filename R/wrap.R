abacd3f1a9f4d2aad2bf316c03fc2810f2c36c21
Dirk Eddelbuettel <edd@debian.org>
2026-06-01 10:34:55 -0500

    Expanded manual page via roxygen section

--- R/wrap.R
+++ R/wrap.R
@@ -1,6 +1,19 @@
-##' Convenience Wrappers for 'spdlite' Logging
+##' @title Logging wrappers for 'spdlite' logging from both R and C++
 ##'
-##' Several short wrappers for functions from 'spdlite' are provided as a convenience.
+##' @description Several wrappers for functions from 'spdlite' are provided as a
+##' convenience. In general, these can be accessed both from C++ (via the
+##' provided C++20 header), and from R via the functions documented here
+##' allowing for consitent logging throughout an R package.
+##'
+##' @details Logging functions respect a global logging level that defaults to \sQuote{info}
+##' (meaning that calls to \code{log_trace} or \code{log_debug} are ignored).
+##' Several formatting options are available as well to control the number of digits
+##' on the timestamp, whether or not the thread id is displayed, whether the date
+##' portion of the timestamp is to be displayed and whether the display is in local
+##' time (the default) or in UTC.
+##'
+##' The C++ functionality is illustrated in the example file in the \code{examples}
+##' directory.
 ##'
 ##' @param level Character value for the logging level
 ##' @param s Character value for filename, pattern, level, or logging message
@@ -11,8 +24,27 @@
 ##' @param utc Boolean flag select display of current time in UTC rather than local,
 ##' default is off
 ##' @param ... Supplementary arguments for the logging string
+##'
 ##' @return Nothing is returned from these functions (with the exception of
 ##' \code{get_level()}) as they are invoked for their side-effects.
+##'
+##' @author Dirk Eddelbuettel
+##'
+##' @examples
+##' lvl <- rspdlite::get_level()
+##' rspdlite::log_debug("This message is ignored by the default level 'info'.")
+##' rspdlite::log_info("This message is show by the default level.")
+##' rspdlite::set_level("warn")
+##' rspdlite::log_info("Now this message at 'info' is ignored too.")
+##' rspdlite::log_warn("A warning messages passes at level warning. {}", 42L)
+##' rspdlite::set_name("my_logger")
+##' rspdlite::log_error("Error messages also pass, and see the name set")
+##' rspdlite::set_format(show_thread_id=TRUE, precision="ns")
+##' rspdlite::log_error("Warning message under changed formatting")
+##' rspdlite::set_level(lvl) # revert to prior level
+##' rspdlite::set_name("") # revert to no name
+##' rspdlite::set_format() # revert to default format
+
 log_trace   <- function(s, ...)  trace_(fmt(s,...))
 
 ##' @rdname log_trace
